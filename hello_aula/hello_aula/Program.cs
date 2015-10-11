c80c32760631fe7876a005ba75784ce79610a7fa
Radoslav Orlinov <radoslav.orlinov@gmail.com>
2015-10-11 16:59:54 +0300

    Създаване на програма Hello Aula

---
+++ hello_aula/hello_aula/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_aula
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Hello Aula");
+			Console.ReadKey ();
+		}
+	}
+}

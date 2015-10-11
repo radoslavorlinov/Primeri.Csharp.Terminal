bf15e66c2f5eb0f9a7347bdfc389e458f38787c6
Radoslav Orlinov <radoslav.orlinov@gmail.com>
2015-10-11 19:26:20 +0300

    Операции + - * / %

--- modul_02/modul_02/Program.cs
+++ modul_02/modul_02/Program.cs
@@ -7,10 +7,35 @@ namespace modul_02
 		public static void Main (string[] args)
 		{
 			//Дефиниране на променливи
+			int a=0; int b; int sum;
 
 			//Събиане на числа
-
+			a=10; b=6; sum=a+b;
 			//Писане в конзола
+			Console.Write(a);
+			Console.Write (" + ");
+			Console.Write (b);
+			Console.Write (" = ");
+			Console.WriteLine(sum);
+
+			//Допълнителни оператори
+			Console.WriteLine("\nUsing of +=");
+			a += b; // a=a+b
+			Console.WriteLine (a);
+
+			Console.WriteLine("\nUsing of -=");
+			a -= b; // a=a-b
+			Console.WriteLine (a);
+
+			Console.WriteLine("\nUsing of *=");
+			a *= b; // a=a*b
+			Console.WriteLine (a);
+
+			Console.WriteLine ("Standartno delene: " + (7 / 3).ToString ()); //->2
+			Console.WriteLine ("Standartno delene: " + (7 % 3).ToString ()); // остатък 1
+
+
+			Console.ReadKey ();
 		}
 	}
 }
